95379f1fc237837aa11ab31755b1e6ee75209804
Erin <enguy4@ocdsb.ca>
2026-02-14 23:22:20 -0500

    BOY DO I HATE JAVA GUIS

--- src/main/java/com/example/MainScreen.java
+++ src/main/java/com/example/MainScreen.java
@@ -5,20 +5,23 @@ import java.awt.Image;
 import java.awt.image.BufferedImage;
 
 import java.io.File;
-import java.io.IOException;
+import java.awt.*;
+import java.io.*;
+import java.awt.event.*;
 
 import javax.imageio.ImageIO;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.ImageIcon;
 
-public class MainScreen {
+public class MainScreen extends JFrame implements ActionListener, KeyListener, MouseListener{
+    private Image backgroundImage;
+    private JPanel backgroundPanel;
 
-    public static void main(String[] args) throws IOException {
-        Inputscreen();
-    }
 
-    public static void Inputscreen() throws IOException {
+    public void Inputscreen() {
         JFrame input = new JFrame();
         input.setVisible(true);
         input.setSize(1850, 1080);
@@ -26,21 +29,76 @@ public class MainScreen {
         JLabel label1 = new JLabel("hihihihihi herroe");
         input.add(label1);
 
-        BufferedImage myImage = ImageIO.read(new File("BL.png"));
-    }
+        backgroundPanel = new BackgroundPanel();
+        
 
-}
 
-class ImagePanel extends JComponent {
-    private Image image;
 
-    public ImagePanel(Image image) {
-        this.image = image;
+
     }
 
     @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        g.drawImage(image, 0, 0, this);
+    public void mouseClicked(MouseEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mouseClicked'");
     }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mousePressed'");
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mouseReleased'");
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mouseEntered'");
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'mouseExited'");
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyPressed'");
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyReleased'");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    }
+
+    class BackgroundPanel extends JPanel{
+        @Override
+        protected void paintComponent(Graphics g){
+            super.paintComponent(g);
+            Image background = new ImageIcon("BL.png").getImage();
+            g.drawImage(background,0,0,getWidth(),getHeight(),this);
+        }
+    }
+
 }
+
